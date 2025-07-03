66db413697a4f2a982ef5d6572ec6897fe271bb0
Clement Mensah <olitech1010@gmail.com>
2025-07-03 00:07:39 +0000

    JWT access token for login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,101 +1,161 @@
-import express from 'express';
-import expressAsyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
-import bcrypt from 'bcrypt'
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
 //@desc Create  User
 //@route Post /api/users/
 //@access public
 const registerUser = expressAsyncHandler(async (req, res) => {
-    const { name, username, email, password } = req.body
-    if (!name || !username || !email || !password) {
-        res.status(400)
-        throw new Error("All fields are required")
-    }
+  const { name, username, email, password } = req.body;
+  if (!name || !username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
 
-    const userAvailable = await User.findOne({email})
-    if (userAvailable) {
-        res.status(400)
-        throw new Error('User already registered')
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered");
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      username,
+      email,
+      password: hashedPassword,
+    });
+    if (user) {
+      res
+        .status(201)
+        .json({
+          _id: user.id,
+          name: user.name,
+          username: user.username,
+          eemail: user.email,
+        });
+    } else {
+      res.status(400);
+      throw new Error("Request is invalid");
     }
-    try {
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const user = await User.create({ name, username, email, password:hashedPassword })
-    res.status(201).json({_id:user.id, name:user.name, username:user.username, eemail:user.email})
-        
-    } catch (error) {
-        res.status(500).json({error:error.message})
-    }
-
-    })
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 //@desc Log in user
 //@route get /api/users/:id
-//@access private 
+//@access private
 const loginUser = expressAsyncHandler(async (req, res) => {
-    
-})
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
 
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    try {
+      const accessToken = jwt.sign(
+        {
+          user: {
+            user: user.name,
+            email: user.email,
+            id: user.id,
+          },
+        },
+        process.env.JWT_SECRETE,
+        { expiresIn: "5m" }
+      );
+      res.status(200).json({accessToken:accessToken});
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(401);
+    throw new Error("Email or password incorrect");
+  }
+});
 
 //@desc Logout user
 //@route get /api/users/:id
-//@access private 
-const logotUser = expressAsyncHandler(async (req, res) => {
-    
-})
-
+//@access private
+const logoutUser = expressAsyncHandler(async (req, res) => {});
 
 //@desc find All Users
 //@route get /api/users/
 //@access private only admin
 
 const getAllUsers = expressAsyncHandler(async (req, res) => {
-    const users = await User.find()
-    res.status(201).json(users)
-
-})
+  const users = await User.find();
+  res.status(201).json(users);
+});
 
 //@desc find User
 //@route get /api/users/:id
-//@access private 
+//@access private
 const getUser = expressAsyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id)
-    if (!user) {
-        res.status(404)
-        throw new Error('User not found')
-    }
-    res.status(200).json(user)
-})
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+  res.status(200).json(user);
+});
+
+//@desc find CURRENNT User
+//@route get /api/users/current
+//@access private
+const currentUser = expressAsyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+  res.status(200).json(user);
+});
 
 //@desc Update User
 //@route PUT /api/users/:id
-//@access private 
+//@access private
 
 const updateUser = expressAsyncHandler(async (res, req) => {
-    const user = User.findOne(req.params.id)
-    if (!user) {
-        res.status(404)
-        throw new Error('User not available')
-    }
+  const user = User.findOne(req.params.id);
+  if (!user) {
+    res.status(404);
+    throw new Error("User not available");
+  }
 
-    const updatedUser = User.findOneAndUpdate(req.params.id, req.body, { new: true })
-    res.status(201).json(updatedUser)
-
-
-})
+  const updatedUser = User.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(201).json(updatedUser);
+});
 
 //@desc delete  User
 //@route Delete /api/users/:id
-//@access private 
+//@access private
 
 const deleteUser = expressAsyncHandler(async (req, res) => {
-    const deletedUser = await User.findByIdAndDelete(req.params.id)
-    if (!deletedUser) {
-        res.status(404)
-        throw new Error('User not found')
-    }
-    res.status(201).json(deletedUser)
-})
+  const deletedUser = await User.findByIdAndDelete(req.params.id);
+  if (!deletedUser) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+  res.status(201).json(deletedUser);
+});
 
-
-
-export {registerUser, loginUser, logotUser, getAllUsers, getUser, updateUser, deleteUser }
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getAllUsers,
+    getUser,
+  currentUser,
+  updateUser,
+deleteUser,
+  
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,25 @@
 import express from 'express';
 
 const router = express.Router();
-import { registerUser, loginUser, getUser, getAllUsers, updateUser, deleteUser } from '../controllers/userController.js';
+import { registerUser, loginUser, logoutUser, getUser, currentUser, getAllUsers, updateUser, deleteUser } from '../controllers/userController.js';
 
 router.route("/")
     .get(getAllUsers)
     .post(registerUser)
 
+
 router.route("/:id")
     .get(getUser)
     .put(updateUser)
     .delete(deleteUser)
+
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(logoutUser)
+router.route("/current").post(currentUser)
+
+
     
 
 
